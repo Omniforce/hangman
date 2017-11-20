da48529292a4e0991e6049cb054067c6aac61dfc
Brendan Ross <brendanros@gmail.com>
2017-11-20 10:16:43 -0500

    Changing some text on the UI

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
   <div className="header">
     <h1 className="title">Hangman</h1>
-    <h4>Try to guess the mystery word! You have 10 attempts. Press a key to make your first guess.</h4>
+    <h4>Try to guess the mystery word! You have 10 attempts. Enter a letter to make your first guess.</h4>
   </div>
 );
 
